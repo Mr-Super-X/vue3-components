c9d3bcf846e1b634cddaa688387ba103bd47ae28
v_jpch <v_jpch.digitalgd.com>
2025-01-21 18:57:12 +0800

    feat: 实现打包源码组件统一入口

--- build/component.ts
+++ build/component.ts
@@ -16,7 +16,7 @@ import fs from 'fs'
 import * as VueCompiler from '@vue/compiler-sfc'
 
 const buildEachComponent = async () => {
-  const files = sync('*', {
+  const files = sync("*", {
     cwd: componentRoot, // 设置目录
     onlyDirectories: true, // 只匹配文件夹
   })
@@ -40,11 +40,11 @@ const buildEachComponent = async () => {
     const outputOptions = Object.values(buildConfig).map(conf => ({
       format: conf.format, // 输出什么格式
       file: path.resolve(conf.output.path, `components/${file}/index.js`), // 输出到目录的某个组件下的index.js
-      paths: pathRewriter(conf.output.name),
+      paths: pathRewriter(conf.output.name)
     }))
 
     // 打包输出
-    await Promise.all(outputOptions.map(output => bundle.write(output as OutputOptions)))
+    await Promise.all(outputOptions.map((output) => bundle.write(output as OutputOptions)))
   })
 
   return Promise.all(builds)
@@ -61,10 +61,9 @@ const genTypes = async () => {
       noEmitOnError: true, // 不抛出错误
       outDir: path.resolve(outDir, 'types'), // 输出目录 dist/types
       baseUrl: projectRoot, // 基础路径
-      paths: {
-        // 解析路径
+      paths: { // 解析路径
         // 所有以@cjp-cli-dev开头的都去packages模块中查找
-        '@cjp-cli-dev/*': ['packages/*'],
+        "@cjp-cli-dev/*": ['packages/*']
       },
       skipLibCheck: true, // 跳过类库检测
       strict: false, // 不要严格模式
@@ -83,46 +82,46 @@ const genTypes = async () => {
   const sourceFiles: SourceFile[] = []
 
   // 遍历所有文件，处理.vue文件和.ts文件
-  await Promise.all(
-    filePaths.map(file => {
-      // 匹配文件是.vue文件和.ts文件，仅处理这两种
-      if (file.endsWith('.vue')) {
-        // 读取.vue文件的内容
-        const source = fs.readFileSync(file, 'utf8')
+  await Promise.all(filePaths.map(file => {
+    // 匹配文件是.vue文件和.ts文件，仅处理这两种
+    if (file.endsWith('.vue')) {
+      // 读取.vue文件的内容
+      const source = fs.readFileSync(file, 'utf8')
 
-        // 解析content
-        const sfc = VueCompiler.parse(source)
-        // 拿到脚本 可能是script lang='ts' 也可能是script setup lang='ts'
-        const { script, scriptSetup } = sfc.descriptor
-        if (script || scriptSetup) {
-          const content = script?.content || scriptSetup?.content // 拿到脚本内容
-          // 判断.vue lang='ts' 将icon.vue.ts => icon.vue.d.ts
-          if (script?.lang === 'ts' || scriptSetup?.lang === 'ts') {
-            // 创建文件
-            const sourceFile = project.createSourceFile(file + '.ts', content)
-            sourceFiles.push(sourceFile)
-          }
+      // 解析content
+      const sfc = VueCompiler.parse(source)
+      // 拿到脚本 可能是script lang='ts' 也可能是script setup lang='ts'
+      const { script, scriptSetup } = sfc.descriptor
+      if (script || scriptSetup) {
+        let content = script?.content || scriptSetup?.content // 拿到脚本内容
+        // 判断.vue lang='ts' 将icon.vue.ts => icon.vue.d.ts
+        if (script?.lang === 'ts' || scriptSetup?.lang === 'ts') {
+          // 创建文件
+          const sourceFile = project.createSourceFile(file + '.ts', content)
+          sourceFiles.push(sourceFile)
         }
-      } else {
-        // 把所有的ts文件都放在一起发射成.d.ts
-        const sourceFile = project.addSourceFileAtPath(file)
-        sourceFiles.push(sourceFile)
       }
-    })
-  )
+    } else {
+      // 把所有的ts文件都放在一起发射成.d.ts
+      const sourceFile = project.addSourceFileAtPath(file)
+      sourceFiles.push(sourceFile)
+    }
+  }))
 
   // 生成声明文件.d.ts 默认是放到内存中的，需要手动写入文件
   await project.emit({
     emitOnlyDtsFiles: true, // 只生成.d.ts文件
   })
 
+
   // 遍历所有文件，手动写入文件
-  const tasks = sourceFiles.map(async sourceFile => {
+  const tasks = sourceFiles.map(async (sourceFile) => {
     // 获取发射脚本
     const emitOutput = sourceFile.getEmitOutput()
 
     // 拿到所有要输出的文件，遍历拿到路径
-    const outputTasks = emitOutput.getOutputFiles().map(async outputFile => {
+    const outputTasks = emitOutput.getOutputFiles().map(async (outputFile) => {
+
       // 拿到路径
       const filePath = outputFile.getFilePath()
 
@@ -145,16 +144,45 @@ const copyTypes = () => {
   // 要拷贝的路径
   const src = path.resolve(outDir, 'types/components/')
 
-  const copy = module => {
+  const copy = (module) => {
     // 拿到出口
-    const output = path.resolve(outDir, module, 'components')
+    let output = path.resolve(outDir, module, 'components')
 
     // 返回执行命令
     return () => run(`cp -r ${src}/* ${output}`)
   }
 
   // 分别拷贝，并行执行
-  return parallel(copy('es'), copy('lib'))
+  return parallel(
+    copy('es'),
+    copy('lib'),
+  )
 }
 
-export const buildComponent = series(buildEachComponent, genTypes, copyTypes())
+// 打包packages/components/index.ts
+const buildComponentEntry = async () => {
+  const config = {
+    input: path.resolve(componentRoot, "index.ts"),
+    plugins: [typescript()],
+    external: () => true,
+  }
+
+  const bundle = await rollup(config)
+
+  return Promise.all(
+    Object.values(buildConfig)
+      .map(conf => ({
+        format: conf.format, // 输出格式
+        // 将packages/components/index.ts打包输出到dist/es/components/index.js  dist/lib/components/index.js下
+        file: path.resolve(conf.output.path, 'components/index.js'),
+      }))
+      .map(output => bundle.write(output as OutputOptions))
+  )
+}
+
+export const buildComponent = series(
+  buildEachComponent,
+  genTypes,
+  copyTypes(),
+  buildComponentEntry,
+)
